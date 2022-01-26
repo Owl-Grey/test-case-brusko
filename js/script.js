973954654aae35eb9ee98f6c6c9f71e9cc9e1e93
Owl-Grey <bruss.serials@gmail.com>
2022-01-26 15:05:07 +0400

    Добавил маску для номера телефона. Добавил указание на ошибки. Поправил косяки в адаптивах. Добавил в отправку метод связи, поправил скрипт просмотра пароля.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('body').on('click', '.password-control', function(){
+$('.password-control').on('click', function(){
   if ($('#InputPassword').attr('type') == 'password'){
     $('#InputPassword').attr('type', 'text');
   } else {
@@ -7,9 +7,7 @@ $('body').on('click', '.password-control', function(){
   return false;
 });
 
-$('.phone').on('focus', function(){
-  if ($(this).val()==""){$(this).val("+7")}
-});
+$('.phone').mask("+79999999999")
 
 
 
@@ -18,10 +16,19 @@ function PhoneValid(){
   phone = $('.phone').val();
   if((mask.test(phone)==true) && (phone.length>0)){
     if ($('.phone').hasClass('error')){ $('.phone').removeClass('error');}
+    if(!$('.phone_input .error_input').hasClass('hidden_block')){ $('.phone_input .error_input').addClass('hidden_block')}
     return true;
     }
   else{
     $('.phone').addClass('error')
+    if(phone.length==0){
+      $('.phone_input .error_input').text("Поле не должно быть пустым");
+      if($('.phone_input .error_input').hasClass('hidden_block')){console.log(1); $('.phone_input .error_input').removeClass('hidden_block');}
+    }
+    else if(!mask.test(phone)){
+      $('.phone_input .error_input').text("Номер должен быть в формате +79999999999");
+      if($('.phone_input .error_input').hasClass('hidden_block')){$('.phone_input .error_input').removeClass('hidden_block');}
+    }
     return false;
   }
 }
@@ -31,24 +38,34 @@ function EmailValid(){
   mail = $('.email').val();
   if((mask.test(mail)==true) && (mail.length>0)){
     if ($('.email').hasClass('error')){ $('.email').removeClass('error');}
+    if(!$('.mail_input .error_input').hasClass('hidden_block')){ $('.mail_input .error_input').addClass('hidden_block')}
     return mask.test(mail);
     }
   else{
-    console.log(1)
     $('.email').addClass('error')
+    if(mail.length==0){
+      $('.mail_input .error_input').text("Поле не должно быть пустым");
+      if($('.mail_input .error_input').hasClass('hidden_block')){$('.mail_input .error_input').removeClass('hidden_block');}
+    }
+    else if(!mask.test(mail)){
+      $('.mail_input .error_input').text("Email должен быть в формате example@domen.ru");
+      if($('.mail_input .error_input').hasClass('hidden_block')){$('.mail_input .error_input').removeClass('hidden_block');}
+    }
     return false;
   }
 }
 
 function PassValid(){
   pass = $('.password').val();
-  if(pass.length>0){
+  if(pass.length>8){
     if ($('.password').hasClass('error')){ $('.password').removeClass('error');}
+    if(!$('.password_input .error_input').hasClass('hidden_block')){ $('.password_input .error_input').addClass('hidden_block')}
     return true;
     }
   else{
-    console.log(2)
     $('.password').addClass('error')
+    $('.password_input .error_input').text("Поле не должно быть пустым");
+    if($('.password_input .error_input').hasClass('hidden_block')){$('.password_input .error_input').removeClass('hidden_block');}
     return false;
   }
 }
@@ -64,23 +81,24 @@ $("#ajax_form").on('submit', function(e) {
   var email = $('#InputEmail').val();
   var phone = $('#InputPhone').val();
   var pass = $('#InputPassword').val();
+  var cont = $('#ContactSelect').val();
   let ph = PhoneValid()
   let em = EmailValid()
   let ps = PassValid()
   if (ph && em && ps){
-    sendAjaxForm(email, phone, pass);
+    sendAjaxForm(email, phone, pass, cont);
   }
   else {
     return false;
   }
 });
 
-function sendAjaxForm(email, phone, pass) {
+function sendAjaxForm(email, phone, pass,cont) {
   $.ajax({
     url: "/smth.php",
     type: "post",
     dataType: "json",
-    data: { "email": email, "phone": phone, "password": pass },
+    data: { "email": email, "phone": phone, "password": pass, "contact": cont },
   }).done(function () {
     console.log("Отправлено!");
   }).fail(function () {
